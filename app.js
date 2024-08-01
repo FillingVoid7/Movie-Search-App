f154b56922f2fc77278cf6b047eca76a86fddda2
FillingVoid7 <sharmabhuwan2000@gmail.com>
2024-08-01 23:45:11 +0545

    add

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const app = express()
 const port  = 3000
 
-const apiKey = '9098d9f9'
+const apiKey = process.env.OMDBapiKey;
 
 app.use(express.static(path.join(__dirname, 'public')))
 
